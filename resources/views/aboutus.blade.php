9bf51c03a699c031e1ea59b9a7e467fcc298d42e
Abhishek <abhishektupudana@gmail.com>
2021-10-12 16:25:25 +0530

    updated a new form

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,7 @@
+<form class="form-inline" action="#">
+  <label for="email">Email:</label>
+  <input type="email" id="email" placeholder="Enter email" name="email">
+  <label for="pwd">Password:</label>
+  <input type="password" id="pwd" placeholder="Enter password" name="pswd">
+  <button type="submit">Submit</button>
+</form>
