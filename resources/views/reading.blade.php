424c3ab33900a2cbe04a2f90e92901d24553b96d
Abhishek <abhishektupudana@gmail.com>
2021-10-13 10:25:34 +0530

    updated

---
+++ resources/views/reading.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Current Reads</title>
+
+    <style>
+      #author {
+        color: blueviolet;
+      }
+      .book-title {
+        color: cadetblue;
+      }
+      .book-author {
+        color: darkgrey;
+      }
+      .book-summary {
+        color: grey;
+      }
+
+      body {
+        background-color: snow;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1>Currently Reading</h1>
+      <h2>Index</h2>
+      <nav>
+        <ul>
+          <li><a href="#">Hundred Years of Solitude</a></li>
+          <li><a href="#">Atomic Habits</a></li>
+          <li><a href="#">Sapiens: A Brief History of Humankind</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <section>
+      <ol>
+        <article>
+          <li><h3 class="book-title">Hundred Years of Solitude</h3></li>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91hJ+hgZm4L.jpg"
+            width="256px"
+            height="400px"
+          />
+          <h4 class="book-author">By Gabriel Garcia Marquez</h4>
+          <p class="book-summary">
+            The theme of this book is about two families that witness various
+            stages of life over the period of a century. How the protagonist try
+            to come to grips with their past and how this obsessiveness brings
+            about the doom of the family is captured in the novel.
+          </p>
+          <a
+            href="https://www.amazon.in/Hundred-Years-Solitude-International-Writers/dp/0140157514"
+            >Buy from Amazon</a
+          >
+        </article>
+
+        <article>
+          <li><h3 class="book-title">Atomic Habits</h3></li>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg"
+            width="256px"
+            height="400px"
+          />
+          <h4 class="book-author">By James Clear</h4>
+          <p class="book-summary">
+            World-renowned habits expert James Clear has discovered a simpler
+            system for transforming your life. He knows that lasting change
+            comes from the compound effect of hundreds of small decisions -
+            doing two push-ups a day, waking up five minutes early, or holding a
+            single short phone call. He calls them atomic habits.
+          </p>
+          <a
+            href="https://www.amazon.in/Atomic-Habits-James-Clear/dp/1847941834"
+            >Buy from Amazon</a
+          >
+        </article>
+
+        <article>
+          <li>
+            <h3 class="book-title">Sapiens: A Brief History of Humankind</h3>
+          </li>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/713jIoMO3UL.jpg"
+            width="256px"
+            height="400px"
+          />
+          <h4 class="book-author">By Yuval Noah Harari</h4>
+          <p class="book-summary">
+            World-renowned habits expert James Clear has discovered a simpler
+            system for transforming your life. He knows that lasting change
+            comes from the compound effect of hundreds of small decisions -
+            doing two push-ups a day, waking up five minutes early, or holding a
+            single short phone call. He calls them atomic habits.
+          </p>
+          <a
+            href="https://www.amazon.in/Sapiens-Humankind-Yuval-Noah-Harari/dp/0099590085"
+            >Buy from Amazon</a
+          >
+        </article>
+      </ol>
+    </section>
+
+    <footer>
+      <h5>Website developed by</h5>
+      <a id="author" href="www.github.com/abhishekrnc">Abhishek Kumar</a>
+    </footer>
+  </body>
+</html>
